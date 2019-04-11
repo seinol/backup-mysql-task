fb408f61b57dabf79e994407eea9229ad8574f2b
Jonas <jonas.hauser@swisscom.com>
2019-04-11 08:20:24 +0200

    Fix bugs and make changes for our usecases

--- README.md
+++ README.md
@@ -1,7 +1,48 @@
-## Backup MySQL on Cloud Foundry
+copied for the first time from https://github.com/making/backup-mysql-task, but now modified for our usecases...
 
-Dump mysql bound to this app and send to S3.
+## DB Dumper
 
-1. create `.s3cfg` (for [`s3cmd`](http://s3tools.org/usage)) on the top of this repository.
-1. bind mysql services to backup (written in [`manifest.yml`](manifest.yml))
-1. set environment variable called `BUCKET_NAME` for S3 (written in [`manifest.yml`](manifest.yml))
+Dump mariadb and mongodb service DB's bound to this app and send to s3 dynamic storage.
+
+ATTENTION: Since a bug has occurred the buckets have to be created manually in advance on the s3!
+
+1. set environment variable called `CRON_EXPRESSION` (written in [`manifest.yml`](manifest.yml)). Outcomment the call of the backup function(dbdumper) in the main.go file, otherwise your app will crash often after step 2 and you aren't able to create your s3 config in the container!
+2. push your app to your Application Cloud (cf push).
+3. create `.s3cfg` (for [`s3cmd`](http://s3tools.org/usage)) and put it on the top of this repository. Do this in your container Linux machine, because there is another version of s3cmd than on your personal computer! Access your container with "cf ssh <myapp>".
+4. bind services to backup (written in [`manifest.yml`](manifest.yml))
+5. revert the comment of the backup function call in the main.go file as done in step 1
+6. push your app again to your Application Cloud
+<br>
+
+### ATTENTION
+ - Only mariadbent and mongodbent are currently supported! It is possible to extend to mongodb-2, but it is not implemented yet!
+
+### Troubleshooting in the container
+run the following command first to set LIB and BIN Paths:
+```sh
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/vcap/deps/0/lib/" && export PATH="$PATH:/home/vcap/deps/0/bin/"
+```
+
+#### s3cmd
+```sh
+s3cmd -c /home/vcap/app/.s3cfg <ls|la|mb...>
+```
+
+#### mysql dump
+```sh
+mysqldump --single-transaction --routines --triggers --no-create-db --skip-add-locks -u <username> --password=<password> -h <hostname> --databases <database>
+```
+
+#### mysql restore
+Please use for mysql restore a tool like adminer or phpmyadmin.
+
+#### mongodb dump
+```sh
+mongodump -u <username> -p <password> --host <replica_set>/<host1><:port1>,<host2><:port2>,<host3><:port3> -d <database> -o <path/to/folder>
+```
+
+#### mongodb restore
+Please use in first case a tool which can restore mongodb archive dumps (p. e. Studio 3T). In second case you can use mongorestore(Attention: use the primary host of the mongodb replica).
+```sh
+mongorestore -u <username> -p <password> -h <host1>:<port1> -d <database> <path/to/folder>
+```
--- main.go
+++ main.go
@@ -2,28 +2,120 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/cloudfoundry-community/go-cfenv"
 	"github.com/robfig/cron"
 )
 
-func mysqlbackup() {
-	cfenv, _ := cfenv.Current()
-	services, _ := cfenv.Services.WithTag("mysql")
-	for _, s := range services {
-		dump := fmt.Sprintf("/home/vcap/app/.apt/usr/bin/mysqldump -u %s --password=%s -h %s --databases %s > /tmp/%s.sql", s.Credentials["username"], s.Credentials["password"], s.Credentials["hostname"], s.Credentials["name"], s.Name)
-		exec.Command("sh", "-c", dump).Output()
-		fmt.Println("Dumped on /tmp/" + s.Name + ".sql")
+func dbdumper() {
+	fmt.Println("starting db dumper")
+
+	env, _ := cfenv.Current()
+	mariadbs, _ := env.Services.WithLabel("mariadbent")
+	mongodbs, _ := env.Services.WithLabel("mongodbent")
+
+	//mysqldump for all mariadbs
+	for _, s := range mariadbs {
+		_, err := exec.Command("sh", "-c", "mkdir /tmp/"+s.Name).Output()
+		if err != nil {
+			log.Fatal("creating tmp dir "+s.Name+" failed: ", err.Error())
+			return
+		}
+		command := fmt.Sprintf("/home/vcap/deps/0/apt/usr/bin/mysqldump --single-transaction --routines --triggers --no-create-db --skip-add-locks -u %s --password=%s -h %s --databases %s > /tmp/%s/%s_%s.sql", s.Credentials["username"], s.Credentials["password"], s.Credentials["hostname"], s.Credentials["name"], s.Name, time.Now().Format("2006-01-02"), s.Name)
+		_, err = exec.Command("sh", "-c", command).Output()
+		if err != nil {
+			fmt.Println("creating "+s.Name+" mysqldump failed: ", err.Error())
+		} else {
+			fmt.Println("dumped on /tmp/" + s.Name + "/" + time.Now().Format("2006-01-02") + "_" + s.Name + ".sql")
+		}
 	}
-	exec.Command("sh", "-c", "/home/vcap/app/.apt/usr/bin/s3cmd -c /home/vcap/app/.s3cfg put /tmp/*.sql s3://"+os.Getenv("BUCKET_NAME")).Output()
+
+	//mongodump for all mongodbs
+	for _, s := range mongodbs {
+		_, err := exec.Command("sh", "-c", "mkdir -p /tmp/"+s.Name).Output()
+		if err != nil {
+			log.Fatal("creating tmp dir for "+s.Name+" failed: ", err.Error())
+			return
+		}
+
+		hosts := strings.Split(s.Credentials["host"].(string), ",")
+		command := fmt.Sprintf("/home/vcap/deps/0/apt/usr/bin/mongodump -u %s -p %s -h %s/%s:%s,%s:%s,%s:%s -d %s -o /tmp/%s", s.Credentials["username"], s.Credentials["password"], s.Credentials["replica_set"], hosts[0], s.Credentials["port"], hosts[1], s.Credentials["port"], hosts[2], s.Credentials["port"], s.Credentials["database"], s.Name)
+		_, err = exec.Command("sh", "-c", command).Output()
+		if err != nil {
+			fmt.Println("creating "+s.Name+" mongodump failed: ", err.Error())
+		} else {
+			fmt.Println("dumped on /tmp/" + s.Name + "_" + time.Now().Format("2006-01-02"))
+		}
+	}
+
+	//create bucket and put to bucket with s3cmd for mariadbs
+	for _, s := range mariadbs {
+
+		//creating buckets automatically by the app is actually not possible because of s3cmd errors: 403 (SignatureDoesNotMatch) - problem unresolved
+		_, err := exec.Command("sh", "-c", "/home/vcap/deps/0/apt/usr/bin/s3cmd -c /home/vcap/app/.s3cfg mb s3://"+s.Name).Output()
+		if err != nil {
+			log.Fatal("creating bucket for "+s.Name+" mariadb failed: ", err.Error())
+			return
+		}
+
+		_, err = exec.Command("sh", "-c", "/home/vcap/deps/0/apt/usr/bin/s3cmd -c /home/vcap/app/.s3cfg put /tmp/"+s.Name+"/*.sql s3://"+s.Name).Output()
+		if err != nil {
+			fmt.Println("putting "+s.Name+" mariadbdump to s3 bucket failed: ", err.Error())
+		} else {
+			fmt.Println("pushed " + s.Name + " mariadb sql dump to s3 dynamic storage")
+		}
+	}
+	//create bucket and put to bucket with s3cmd for mongodbs
+	for _, s := range mongodbs {
+
+		//creating buckets automatically by the app is actually not possible because of s3cmd errors: 403 (SignatureDoesNotMatch) - problem unresolved
+		_, err := exec.Command("sh", "-c", "/home/vcap/deps/0/apt/usr/bin/s3cmd -c /home/vcap/app/.s3cfg mb s3://"+s.Name).Output()
+		if err != nil {
+			log.Fatal("creating bucket for "+s.Name+" mongodb failed: ", err.Error())
+			return
+		}
+		_, err = exec.Command("sh", "-c", "cd /tmp/"+s.Name+"/"+s.Credentials["database"].(string)+"/ && rename 's/(.*)$/"+time.Now().Format("2006-01-02")+"_$1/' *.* && cd").Output()
+		if err != nil {
+			fmt.Println("renaming "+s.Name+" dump files failed: ", err.Error())
+		}
+		_, err = exec.Command("sh", "-c", "/home/vcap/deps/0/apt/usr/bin/s3cmd -c /home/vcap/app/.s3cfg put /tmp/"+s.Name+"/"+s.Credentials["database"].(string)+"/* s3://"+s.Name).Output()
+		if err != nil {
+			fmt.Println("putting "+s.Name+" mongodb dump files to s3 bucket failed: ", err.Error())
+		} else {
+			fmt.Println("pushed " + s.Name + " mongodb dump files to s3 dynamic storage")
+		}
+	}
+
+	//delete all local mariadb dumps
+	for _, s := range mariadbs {
+		_, err := exec.Command("sh", "-c", "rm -rf /tmp/"+s.Name).Output()
+		if err != nil {
+			log.Fatal("removing tmp-dir for "+s.Name+" failed: ", err.Error())
+			return
+		}
+		fmt.Println("deleted " + s.Name + " local mariadb sql dump in /tmp")
+	}
+	//delete all local mongodb dumps
+	for _, s := range mongodbs {
+		_, err := exec.Command("sh", "-c", "rm -rf /tmp/"+s.Name).Output()
+		if err != nil {
+			log.Fatal("removing tmp-dir for "+s.Name+" failed: ", err.Error())
+			return
+		}
+		fmt.Println("deleted " + s.Name + " local mongodb dump in /tmp")
+	}
+	fmt.Println("finished, waiting for next cron time")
 }
 
 func main() {
 	c := cron.New()
-	c.AddFunc("@daily", mysqlbackup)
+	c.AddFunc(os.Getenv("CRON_EXPRESSION"), dbdumper)
 	c.Start()
 
 	var port string
